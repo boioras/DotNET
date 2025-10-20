918980e9a40d5f022481fc101f10dc6affce6ca2
boioras <or45.r4yblox@gmail.com>
2025-10-20 21:35:58 +1100

    priority inheritance + overriding

--- ToDoListProgram/obj/Debug/net9.0/ToDoListProgram.AssemblyInfo.cs
+++ ToDoListProgram/obj/Debug/net9.0/ToDoListProgram.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoListProgram")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd0fa323aebdaf5a9978435122fc26dd6bf7ed42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf88836b11818cab3a740cf6bcfd6d32254c6371")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoListProgram")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoListProgram")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
